8264844766e80d738a624110b014f30e35a6e30b
Rob Remington <robrem@users.noreply.github.com>
2017-05-25 15:44:22 -0700

    basic functionality and testing for q, state, ntee, c_code params

--- nonprofit.py
+++ nonprofit.py
@@ -4,7 +4,6 @@ A Python client for the ProPublica Nonprofit Explorer API.
 API docs: https://www.propublica.org/datastore/api/nonprofit-explorer-api
 """
 
-import os
 import json
 import httplib2
 import urllib
@@ -39,20 +38,46 @@ class Client(object):
 
 class SearchClient(Client):
 
-    def get(self):
+    def get(self, **kwargs):
         """
             Returns a list of organizations matching the given search terms.
         """
-        path = "search.json"
+
+        """
+            TODO:
+            - regular search: q='terms'
+            - "exact" search appears to make no difference
+            - exact order: exact='terms'
+            - and: require=['term1', 'term2', 'etc']
+            - exclude: exclude='terms'
+            - state id: state='state_id'
+        """
+        params = {}
+        for key, value in kwargs.iteritems():
+
+            if key == 'state':
+                key = 'state[id]'
+
+            if key == 'ntee':
+                key = 'ntee[id]'
+
+            if key == 'c_code':
+                key = 'c_code[id]'
+
+            params[key] = value
+
+        params = urllib.urlencode(params)
+        path = 'search.json?%s' % (params)
+        return self.fetch(path)
 
 
 class OrgsClient(Client):
 
     def get(self, ein):
         """ Returns an organization object for the given employer
-            identification number (EIN).
+            identification number (ein).
         """
-        path = "organizations/{0}.json".format(ein)
+        path = 'organizations/{0}.json'.format(ein)
         return self.fetch(path)
 
 
--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import unittest
-import os
 import json
 import httplib2
 import urllib
 from nonprofit import Nonprofit, NonprofitError
 
-# Propublica employer identification number
+# Test case
 EIN = '142007220'
+ORG = 'Delta'
+US_STATE = 'CA'
+NTEE = 7     # Public, Societal Benefit
+C_CODE = 3   # 501(c)(3)
+
 
 class NonprofitTest(unittest.TestCase):
 
@@ -22,6 +26,37 @@ class NonprofitTest(unittest.TestCase):
 
 class OrgsTest(NonprofitTest):
 
+    def test_get_search_term(self):
+        result = self.nonprofit.search.get(q=ORG)
+        url = "https://projects.propublica.org/nonprofits/api/v2/search.json?q={0}".format(ORG)
+        self.check_response(result, url)
+
+
+    def test_get_search_state(self):
+        result = self.nonprofit.search.get(state=US_STATE)
+        url = "https://projects.propublica.org/nonprofits/api/v2/search.json?state%5Bid%5D={0}".format(US_STATE)
+        self.check_response(result, url)
+
+
+    def test_get_search_ntee(self):
+        result = self.nonprofit.search.get(ntee=NTEE)
+        url = "https://projects.propublica.org/nonprofits/api/v2/search.json?ntee%5Bid%5D={0}".format(NTEE)
+        self.check_response(result, url)
+
+
+    def test_get_search_c_code(self):
+        result = self.nonprofit.search.get(c_code=C_CODE)
+        url = "https://projects.propublica.org/nonprofits/api/v2/search.json?c_code%5Bid%5D={0}".format(C_CODE)
+        self.check_response(result, url)
+
+
+    def test_get_search_all(self):
+        result = self.nonprofit.search.get(q=ORG, state=US_STATE, ntee=NTEE, c_code=C_CODE)
+        url = "https://projects.propublica.org/nonprofits/api/v2/search.json?q={0}&state%5Bid%5D={1}&ntee%5Bid%5D={2}&c_code%5Bid%5D={3}"
+        url = url.format(ORG, US_STATE, NTEE, C_CODE)
+        self.check_response(result, url)
+
+
     def test_get_org(self):
         org = self.nonprofit.orgs.get(EIN)
         url = "https://projects.propublica.org/nonprofits/api/v2/organizations/{0}.json".format(EIN)
@@ -30,3 +65,4 @@ class OrgsTest(NonprofitTest):
 
 if __name__ == "__main__":
     unittest.main()
+    
